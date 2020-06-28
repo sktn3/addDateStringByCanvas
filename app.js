14896f7c8004f6c0c81d6f997dcb665afeb81399
sktn3 <sakatani@gmail.com>
2020-06-28 15:17:00 +0900

    マルチファイル対応、type=fileタグのcss変更
    ・iPhoneだとブラウザに画像を渡した時点でexif情報が消されているっぽい？

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
+'use strict';
+
 // 定数定義
 
+
 // オブジェクト
-const Banner = {
-  font: 'bold 42px serif', // フォント
-  fontcolor: 'red', // 文字色
-  text: getYMD(new Date()), // テキスト
-
-  board: null,
-  reader: null,
-
-  // Canvas情報
-  canvas: {
-    width: null, // 横幅
-    height: null, // 高さ
-    ctx: null // context
-  }
+const Font = {
+  font: 'bold {{}}px serif', // フォント
+  fontcolor: 'red' // 文字色
 }
 
 function getYMD(d){
@@ -25,50 +17,74 @@ function getYMD(d){
         //[ "日", "月", "火", "水", "木", "金", "土" ][d.getDay()] ;    // 曜日(日本語表記)
 }
 
+function msg(str){
+    document.getElementById("msg").innerHTML = str;
+}
+
 // [event] ページ読み込み完了
 window.onload = function(e){
-  const message = document.querySelector('#txt-message'); // テキストボックス
-  const colorText = document.querySelector('#color-text'); // 文字色
-  const colorBg = document.querySelector('#color-bg'); // 背景色
-
-  // Canvasの情報を代入
-  const board = document.querySelector('#board');
-  Banner.canvas.ctx = board.getContext('2d');
-  Banner.canvas.width = board.width;   // 横幅
-  Banner.canvas.height = board.height;  // 高さ
-  Banner.board = board;
+    const message = document.querySelector('#txt-message'); // テキストボックス
+    const colorText = document.querySelector('#color-text'); // 文字色
+    const colorBg = document.querySelector('#color-bg'); // 背景色
 
     document.getElementById("file").addEventListener('change', function(e){
-        const canvas = Banner.board;
-        const ctx = Banner.canvas.ctx;
-        const reader = new FileReader();
-        const img = document.getElementById("img");
 
-        // EXIF.getDataでexif情報を解析
-        EXIF.getData(e.target.files[0], function() {
-            Banner.text = (EXIF.getTag(this, "DateTimeOriginal")).split(" ")[0].replace(/:/g,"/");
-        });
+        const files = e.target.files;
+        console.log("file num: "+files.length);
+        let readers = new Array();
+        let dateStrs = new Array();
 
-        reader.onload = function(event) {
-            let image = new Image();
-            image.onload = function() {
-                canvas.width  = image.naturalWidth;
-                canvas.height = image.naturalHeight;
+        for(let i=0; i<files.length; i++){
+            // EXIF.getDataでexif情報を解析
+            EXIF.getData(files[i], function() {
+                const date = EXIF.getTag(this, "DateTimeOriginal");
+                if(date){
+                    dateStrs.push( date.split(" ")[0].replace(/:/g,"/"));
+                    console.log(dateStrs[i]);
+                }else{
+                    msg("Exif情報がありません。本日の日付を入れます");
+                    dateStrs.push( getYMD(new Date()));
+                    console.log(dateStrs[i]);
+                }
+            });
+        
+            readers[i] = new FileReader();
 
-                // 画像を表示
-                ctx.drawImage(image, 0, 0);
+            readers[i].onload = function(event) {
+                let image = new Image();
+                image.onload = function() {
+                    //canvas と img の作成
+                    // Canvasの情報を代入
+                    const view = document.querySelector('#view');
+                    const canvases = [];
+                    const imgs = [];
+                    canvases[i] = document.createElement("canvas");
+                    imgs[i] = document.createElement("img");
+                    view.appendChild(canvases[i]);
+                    view.appendChild(imgs[i]);
 
-                // 文字を描画
-                ctx.font = Banner.font;
-                ctx.fillStyle = Banner.fontcolor;
-                ctx.fillText(Banner.text, canvas.width-300, 40, canvas.width);
+                    canvases[i].ctx = canvases[i].getContext('2d');
 
-                img.src = canvas.toDataURL();
-                canvas.style.display = "none";
+                    canvases[i].width  = image.naturalWidth;
+                    canvases[i].height = image.naturalHeight;
+
+                    // 画像を表示
+                    canvases[i].ctx.drawImage(image, 0, 0);
+
+                    // 文字を描画
+                    const fontSize = Math.round(canvases[i].height/24)
+                    const strH = Math.round(fontSize*1.3);
+                    canvases[i].ctx.font = Font.font.replace("{{}}",fontSize);
+                    canvases[i].ctx.fillStyle = Font.fontcolor;
+                    canvases[i].ctx.fillText(dateStrs[i], 40, strH, canvases[i].width);
+
+                    imgs[i].src = canvases[i].toDataURL("image/jpeg");
+                    canvases[i].style.display = "none";
+                }
+                image.src = event.target.result;
             }
-            image.src = event.target.result;
+            readers[i].readAsDataURL(e.target.files[i]);
         }
-        reader.readAsDataURL(e.target.files[0]);
 
     });
 }
